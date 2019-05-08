d31a638654b8e34bd06966b91740c47e318b176d
Harald Göthblad <haraldgothblad@Haralds-iMac.lan>
2019-05-08 12:41:08 +0200

    Fixed

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function calculate() {
+function calc() {
     var a = parseInt(document.querySelector("#value1").value);
     var b = parseInt(document.querySelector("#value2").value);
     var op = document.querySelector("#operator").value;
